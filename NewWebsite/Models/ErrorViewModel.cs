83a4bd18ebee80424b4f112bb7e649c76e879ef1
ESK <ESK@DESKTOP-21A7SAV>
2024-06-05 11:35:48 +0300

    Add project files.

---
+++ NewWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
