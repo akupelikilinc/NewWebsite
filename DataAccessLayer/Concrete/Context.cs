b9891026fa0fe871fe3df256b112ffd944118f0d
Ahmet KÜPELİKILINÇ <ahmetkupelikilinc@hotmail.com>
2024-06-06 17:34:27 +0300

    Entity Oluşturuldu.

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring (DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer("server=DESKTOP-21A7SAV; database=NewMyWebsite;integrated security=true");
+        }
+    }
+}
